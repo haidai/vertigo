0197d13c24028b5317da38681ab254081bb04fbf
Hai Nguyen <hai@mayfieldrobotics.com>
2016-08-03 11:34:18 -0700

    add case to not ask for estimate of init pose (which caused a crash)

--- examples/robustISAM2/robustISAM2.cpp
+++ examples/robustISAM2/robustISAM2.cpp
@@ -5,7 +5,6 @@
  *      Author: niko
  */
 
-//#include <gtsam/slam/planarSLAM.h>
 #include <gtsam/geometry/Pose2.h>
 #include <gtsam/nonlinear/ISAM2.h>
 
@@ -20,27 +19,14 @@
 #include <gtsam/slam/PriorFactor.h>
 #include <gtsam/slam/BetweenFactor.h>
 
-// additional boost features
-#include "boost/program_options.hpp"
-namespace po = boost::program_options;
-
 #include "boost/foreach.hpp"
 #define foreach BOOST_FOREACH
 
 #include <fstream>
-//#include <boost/progress.hpp>
-#include <boost/lexical_cast.hpp>
-
 
 #include "BetweenFactorHai.h"
-//#include "betweenFactorSwitchable.h"
-//#include "switchVariableLinear.h"
-//#include "switchVariableSigmoid.h"
-//#include "betweenFactorMaxMix.h"
 #include "timer.h"
 
-//using namespace vertigo;
-//using namespace gtsam;
 using namespace std;
 
 
@@ -59,32 +45,6 @@ struct Edge {
   double weight;
 };
 
-
-
-//void writeResults(gtsam::Values &results, std::string outputFile)
-//{
-//    std::ofstream resultFile(outputFile.c_str());
-//
-//    gtsam::Values::ConstFiltered<gtsam::Pose2> result_poses = results.filter<gtsam::Pose2>();
-//    foreach (const gtsam::Values::ConstFiltered<gtsam::Pose2>::KeyValuePair& key_value, result_poses) {
-//        gtsam::Pose2 p = key_value.value;
-//        resultFile << "VERTEX_SE2 " << p.x() << " " << p.y() << " " << p.theta() << endl;
-//    }
-//
-//    {
-//        gtsam::Values::ConstFiltered<SwitchVariableLinear> result_switches = results.filter<SwitchVariableLinear>();
-//        foreach (const gtsam::Values::ConstFiltered<SwitchVariableLinear>::KeyValuePair& key_value, result_switches) {
-//            resultFile << "VERTEX_SWITCH " << key_value.value.value() << endl;
-//        }
-//    }
-//
-//    {
-//        gtsam::Values::ConstFiltered<SwitchVariableSigmoid> result_switches = results.filter<SwitchVariableSigmoid>();
-//        foreach (const gtsam::Values::ConstFiltered<SwitchVariableSigmoid>::KeyValuePair& key_value, result_switches) {
-//            resultFile << "VERTEX_SWITCH " << key_value.value.value() << endl;
-//        }
-//    }
-//}
 
 // ===================================================================
 bool parseDataset(string inputFile, vector<Pose>&poses, vector<Edge>&edges,multimap<int, int> &poseToEdges)
@@ -188,12 +148,17 @@ int main(int argc, char *argv[])
     gtsam::Values initialEstimate;
     int switchCounter = -1;
 
-    // build problem
+    printf("Number of poses %d\n", poses.size());
+    printf("Number of edges %d\n", edges.size());
+
+    // Build problem
+    // Loop through each pose
     for (int i = 0; i < poses.size(); i++) {
         auto p = poses[i];
         auto edgesFromPose = poseToEdges.equal_range(p.id);
         printf("Pose %d: \n", p.id);
 
+        // Loop through each edge associated with that pose
         for (auto it = edgesFromPose.first; it != edgesFromPose.second; it++) {
             auto edge = edges[it->second];
             printf("    edge: %d -- %d (%.2f %.2f %.2f)\n", edge.i, edge.j,
@@ -203,18 +168,29 @@ int main(int argc, char *argv[])
             printf("        weight: %.3f\n", edge.weight);
             //std::cout <<  "        cov:\n[" << edge.covariance << "]" << std::endl;
 
+            // If this is an odometry edge
             if (edge.j == edge.i + 1) {
-                gtsam::Pose2 predecessorPose = isam.calculateEstimate<gtsam::Pose2>(gtsam::Symbol('x', p.id - 1));
-                if (std::isnan(predecessorPose.x()) || std::isnan(predecessorPose.y()) || std::isnan(predecessorPose.theta())) {
-                    std::cout << "! Degenerated solution (NaN) detected. Solver failed." << std::endl;
-                    //writeResults(globalInitialEstimate, outputFile);
-                    return 0;
+
+                // If it links to the first pose, since we can't incrementally solve for the first pose
+                // (iSAM needs at least two factors on a variable), we can't ask
+                // for a smoothed estimate of it yet
+                if (edge.i == 0) {
+                    initialEstimate.insert(gtsam::Symbol('x', p.id), gtsam::Pose2(edge.x, edge.y, edge.th));
+
+                } else {
+                    gtsam::Pose2 predecessorPose = isam.calculateEstimate<gtsam::Pose2>(gtsam::Symbol('x', p.id - 1));
+                    if (std::isnan(predecessorPose.x()) || std::isnan(predecessorPose.y()) || std::isnan(predecessorPose.theta())) {
+                        std::cout << "! Degenerated solution (NaN) detected. Solver failed." << std::endl;
+                        //writeResults(globalInitialEstimate, outputFile);
+                        return 0;
+                    }
+
+                    initialEstimate.insert(gtsam::Symbol('x', p.id), predecessorPose * gtsam::Pose2(edge.x, edge.y, edge.th));
                 }
 
-                initialEstimate.insert(gtsam::Symbol('x', p.id), predecessorPose * gtsam::Pose2(edge.x, edge.y, edge.th));
                 //globalInitialEstimate.insertPose(p.id, predecessorPose * Pose2(e.x, e.y, e.th));
-
                 gtsam::SharedNoiseModel odom_model = gtsam::noiseModel::Gaussian::Covariance(edge.covariance);
+                printf("graph: Odom BetweenFactor x%d -- x%d\n", edge.i, edge.j);
                 graph.add(gtsam::BetweenFactor<gtsam::Pose2>(
                             gtsam::Symbol('x', edge.i), gtsam::Symbol('x', edge.j),
                             gtsam::Pose2(edge.x, edge.y, edge.th),
@@ -227,12 +203,14 @@ int main(int argc, char *argv[])
                 // create switch prior factor
                 gtsam::SharedNoiseModel switchPriorModel = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector1(1.0));
 
+                printf("graph: Switch Prior s%d\n", switchCounter);
                 graph.add(gtsam::PriorFactor<gtsam::Vector1>(
                             gtsam::Symbol('s', switchCounter),
                             gtsam::Vector1(1.0),
                             switchPriorModel));
 
                 // create switchable odometry factor
+                printf("graph: Switch BetweenFactor x%d -- x%d -- s%d\n", edge.i, edge.j, switchCounter);
                 gtsam::SharedNoiseModel odom_model = gtsam::noiseModel::Gaussian::Covariance(edge.covariance);
                 graph.add(gtsam::BetweenFactorHai<gtsam::Pose2>(
                             gtsam::Symbol('x', edge.i),
@@ -246,8 +224,8 @@ int main(int argc, char *argv[])
             printf("    init: %d\n", i);
             // add prior for first pose
             gtsam::noiseModel::Diagonal::shared_ptr prior_model = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector3::Constant(0.1));
-            graph.push_back(
-                    gtsam::PriorFactor<gtsam::Pose2>(
+            printf("graph: x0 prior\n");
+            graph.add(gtsam::PriorFactor<gtsam::Pose2>(
                         gtsam::Symbol('x', 0),
                         gtsam::Pose2(p.x, p.y, p.th),
                         prior_model));
@@ -255,16 +233,20 @@ int main(int argc, char *argv[])
             // initial value for first pose
             initialEstimate.insert(gtsam::Symbol('x', 0), gtsam::Pose2(p.x, p.y, p.th));
         } else {
+            printf("isam: update with estimate\n");
             isam.update(graph, initialEstimate);
+            printf("isam: calculate\n");
             isam.update();
             gtsam::Values currentEstimate = isam.calculateEstimate();
-            std::cout << "****************************************************" << std::endl;
-            std::cout << "Iteration " << i << ": " << std::endl;
-            currentEstimate.print("Current estimate: ");
+
+            if (i % 100 || i == std::max(poses.size() - 1, size_t(0))) {
+                std::cout << "****************************************************" << std::endl;
+                std::cout << "Iteration " << i << ": " << std::endl;
+                currentEstimate.print("Current estimate: ");
+            }
 
             graph.resize(0);
             initialEstimate.clear();
-            //df
         }
     }
 
